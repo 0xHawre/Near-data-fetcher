a965c5214aaf7a9a56e8261b33c8484593834ca3
Hawre <131952165+Hafxhak@users.noreply.github.com>
2024-05-24 03:56:16 +0330

    Update near.py

--- near.py
+++ near.py
@@ -28,20 +28,7 @@ if __name__ == "__main__":
     urls = [
         "https://near.lava.build/lava-referer-b3ebd262-006d-4555-8273-6d1508c4e92d/",
         "https://near-testnet.lava.build/lava-referer-b3ebd262-006d-4555-8273-6d1508c4e92d/",
-        "https://near.lava.build/lava-referer-a141e6b6-3c73-4556-be82-a04d6f219e8e/",
-        "https://near-testnet.lava.build/lava-referer-a141e6b6-3c73-4556-be82-a04d6f219e8e/",
-        "https://near.lava.build/lava-referer-fbd614bb-40a2-46c0-9f86-675e78683794/",
-        "https://near-testnet.lava.build/lava-referer-fbd614bb-40a2-46c0-9f86-675e78683794/",
-        "https://near.lava.build/lava-referer-fd9e82b1-645a-472f-b741-3c15df5dba76/",
-        "https://near-testnet.lava.build/lava-referer-fd9e82b1-645a-472f-b741-3c15df5dba76/",
-        "https://near.lava.build/lava-referer-b1c0bcc3-6f5a-4b6d-afc6-abf871cb75e5/",
-        "https://near-testnet.lava.build/lava-referer-b1c0bcc3-6f5a-4b6d-afc6-abf871cb75e5/",
-        "https://near.lava.build/lava-referer-091c45cb-8147-4d88-bcb8-999809246d46/",
-        "https://near-testnet.lava.build/lava-referer-091c45cb-8147-4d88-bcb8-999809246d46/",
-        "https://near.lava.build/lava-referer-9bb70aef-60be-4e76-b14e-95f2a45ed686/",
-        "https://near-testnet.lava.build/lava-referer-9bb70aef-60be-4e76-b14e-95f2a45ed686/",
-        "https://near.lava.build/lava-referer-21317879-982f-4a72-97fc-4a291e04c123/",
-        "https://near-testnet.lava.build/lava-referer-21317879-982f-4a72-97fc-4a291e04c123/"
+   
     ]
 
     while True:
